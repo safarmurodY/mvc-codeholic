631bc4c8623f093b649d1f46c160a0d562431a7d
cube <asd@asd.com>
2022-06-14 11:07:46 +0500

    Autoload

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+
+
+use app\core\Application;
+
+require_once __DIR__ . '/vendor/autoload.php';
+$app = new Application();
+
+$app->router->get('/', function (){
+    return 'Hello';
+});
+
+$app->run();

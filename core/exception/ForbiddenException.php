53c37ae8c8270ef041ab3336cb48f755f04e30fa
cube <safarmurod20014@gmail.com>
2022-06-23 13:34:24 +0500

    Core deleted

--- core/exception/ForbiddenException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace app\core\exception;
-
-class ForbiddenException extends \Exception
-{
-    protected $message = 'You DO NOT HAVE PERMISSION';
-    protected $code = 403;
-}
--- core/exception/NotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace app\core\exception;
-
-class NotFoundException extends \Exception
-{
-    protected $message = 'Page not FOUND';
-    protected $code = 404;
-}
